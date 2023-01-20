2907c7776385f7512f51779df6dd868c382f4233
Nihal N <nihalnclt@gmail.com>
2023-01-20 19:31:06 +0530

    file name changed

--- src/admin/models/index.js
+++ src/admin/models/index.js
@@ -1,6 +1,6 @@
 const Admin = require("./admin.model");
 const EmailService = require("./emailService.model");
-const EmailSendList = require("./emailSendList.model");
+const EmailSendList = require("./EmailSendList.model");
 const EmailSettings = require("./emailSettings.model");
 const Email = require("./email.model");
 
