2b4b1c7730a80335a4ff26a5038333c8e55ddb7d
Nihal <nihalnclt@gmail.com>
2023-02-04 11:19:59 +0530

    hotel contract controllers and routes added

--- src/admin/validations/admHotelContract.schema.js
+++ src/admin/validations/admHotelContract.schema.js
@@ -4,11 +4,24 @@ const hotelContractSchema = Joi.object({
     dateFrom: Joi.date().required(),
     dateTo: Joi.date().required(),
     roomType: Joi.string().required(),
-    price: Joi.number().required(),
     contractType: Joi.string()
         .valid("free-sale", "stop-sale", "contracted-rates")
         .required(),
-    isNewUpdate: Joi.boolean().required(),
+    price: Joi.number().allow("", null).when("contractType", {
+        is: Joi.string().valid("free-sale", "contracted-rates"),
+        then: Joi.number().required(),
+    }),
+    // isRoPriceAvailable: Joi.boolean().required(),
+    // roPrice: Joi.number().when("isRoPriceAvailable", {
+    //     is: Joi.boolean().valid(true),
+    //     then: Joi.number().required(),
+    // }),
+    // isBbPriceAvailable: Joi.boolean().required(),
+    // bbPrice: Joi.number().when("isBbPriceAvailable", {
+    //     is: Joi.boolean().valid(true),
+    //     then: Joi.number().required(),
+    // }),
+    // isNewUpdate: Joi.boolean().required(),
 });
 
 module.exports = { hotelContractSchema };
